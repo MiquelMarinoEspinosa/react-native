4a37ee1741b944853db0b3732061654eb7b779aa
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-06-26 10:05:51 +0000

    styling part 2

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -3,12 +3,29 @@ import { View, Text, StyleSheet } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>StyleSheet API</Text>
+      {/* <Text style={styles.title}>StyleSheet API</Text> */}
+      <View style={[styles.lightblueBg, styles.box]}>
+        <Text>Lightblue box</Text>
+      </View>
+      <View style={[styles.box, styles.lightgreenBg]}>
+        <Text>Lightgreen box</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: "plum", padding: 60 },
-  title: {},
+  box: {
+    width: 100,
+    height: 100,
+    padding: 10,
+    backgroundColor: "pink",
+  },
+  lightblueBg: {
+    backgroundColor: "lightblue",
+  },
+  lightgreenBg: {
+    backgroundColor: "lightgreen",
+  },
 });
